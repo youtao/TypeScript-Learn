1282f73a69c253f73df009f24326d8f4c023dc36
youtao <lytlytlc@gmail.com>
2016-01-23 17:38:38 +0800

    添加Modules

---
+++ TypeScript-Learn/Modules/app.ts
@@ -0,0 +1,32 @@
+﻿module Validation {
+    export interface IStringValidator {
+        isAcceptable(s: string): boolean;
+    }
+
+    var lettersRegexp = /^[A-Za-z]+$/;
+    var numberRegexp = /^[0-9]+$/;
+
+    export class LettersOnlyValidator implements IStringValidator {
+        isAcceptable(s: string) {
+            return lettersRegexp.test(s);
+        }
+    }
+
+    export class ZipCodeValidator implements IStringValidator {
+        isAcceptable(s: string) {
+            return s.length === 5 && numberRegexp.test(s);
+        }
+    }
+}
+
+var strings = ['Hello', '98052', '101'];
+var validators: { [s: string]: Validation.IStringValidator; } = {};
+validators['ZIP code'] = new Validation.ZipCodeValidator();
+validators['Letters only'] = new Validation.LettersOnlyValidator();
+strings.forEach(s => {
+    for (var name in validators) {
+        if (validators.hasOwnProperty(name)) {
+            console.log('"' + s + '" ' + (validators[name].isAcceptable(s) ? ' matches ' : ' does not match ') + name);
+        }
+    }
+});
